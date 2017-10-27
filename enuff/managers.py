010a674da43a6e490f11cf382902cf0d6954f4da
yeago <yeago999@gmail.com>
2017-10-27 07:54:28 -0700

    cool snarky comment brah, write better code

--- enuff/managers.py
+++ enuff/managers.py
@@ -27,28 +27,25 @@ class EnuffManager(models.Manager):
     def base_qs(self):
         return self.model.objects.all()
 
-    def get_list(self, queue, as_model=True, limit=None, in_pks=None, randomize=False, site=None, sort_lambda=None):
+    def get_list(self, queue, as_model=True, limit=None, in_pks=None, randomize=False, site=None):
         backend = RedisBackend()
         pks = backend.get_ids(self.get_key(queue, site=site), limit=limit)
         if in_pks:
             pks = [i for i in pks if i in in_pks]
         if not pks:
             return []
-        if randomize:
-            random.shuffle(pks)
         if not as_model:
             return pks
-
-        def generator_inner(qs):
+        def generator_inner(inner_pks):
             count = 0
-            for pk in pks:
-                for instance in qs:
-                    if limit and count == limit:
-                        break
-                    if instance.pk == pk:
-                        count += 1
-                        yield instance
-
-        qs = self.base_qs().filter(pk__in=pks)  # so ok, this is technically SQL but can SQL fuck this one up THAT bad??
-        qs = qs if not sort_lambda else sort_lambda(qs) 
-        return generator_inner(qs)
+            if randomize:
+                random.shuffle(inner_pks)
+            for pk in inner_pks:
+                try:
+                    yield self.base_qs().get(pk=pk)
+                    count += 1
+                except self.model.DoesNotExist:
+                    continue
+                if limit and count == limit:
+                    break
+        return generator_inner(pks)
