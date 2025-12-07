b44fd664fb6d417e82ffba0b1187a7bff6aee510
steve yeago <153018+yeago@users.noreply.github.com>
2025-12-06 21:35:26 -0500

    Update managers.py

--- enuff/managers.py
+++ enuff/managers.py
@@ -28,7 +28,7 @@ class EnuffManager(models.Manager):
         if trim and known_length > trim:
             backend.trim(key, trim)
         if expiry is not None:
-            backend.expire(key, expiry)
+            backend._conn.expire(key, expiry)
 
     def base_qs(self):
         return self.model.objects.all()
